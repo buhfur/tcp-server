50d9bf49521eeade6d6e87823469bd887faa8920
buhfur <ryanmcvicker@protonmail.com>
2025-01-05 18:17:58 -0500

    Auto Commit : Sun Jan  5 06:17:58 PM EST 2025

--- tcp-server/server.py
+++ tcp-server/server.py
@@ -8,12 +8,14 @@
 import socket 
 import struct 
 import logging 
+import random
+from tcp import TCPPacket
 
 # TODO: Only handle the tcp handshake process, respond with ACK + SYN 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-recv_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW,socket.IPPROTO_TCP)
+#recv_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW,socket.IPPROTO_TCP)
 
 def recv_tcp_seg():
     rcv_sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
@@ -21,10 +23,11 @@ def recv_tcp_seg():
 
     while True:
 
+        # TODO : send SYN+ACK 
         ### RECEIVE IP PACKET ###
-        raw_data, addr = recv_socket.recvfrom(65535) 
+        raw_data, addr = rcv_sock.recvfrom(65535) 
         # IP header is the first 20 bytes 
-        ep_header = raw_data[:20]
+        ip_header = raw_data[:20]
         ip_hdr = struct.unpack("!BBHHHBBH4s4s", ip_header)
         protocol = ip_hdr[6] # Protocol number 
         src_ip = socket.inet_ntoa(ip_hdr[8]) # Source IP 
@@ -33,26 +36,35 @@ def recv_tcp_seg():
         # Check if packet is TCP segment 
         if protocol == socket.IPPROTO_TCP:
             ### RECEIVE TCP SEGEMENT ### 
-            # Unpack TCP header ( next 20 bytes after IP header )
-            tcp_header = raw_data[20:40]
+            tcp_header = raw_data[20:40] # Unpack TCP header ( next 20 bytes after IP header )
             tcp_hdr = struct.unpack("!HHLLBBHHH", tcp_header) # Change middle L's to I's if problems arise with transmission 
             # First 16 ( 2 bytes )bits is the source port 
             tcp_dst_port = tcp_hdr[0]
             tcp_src_port = tcp_hdr[1]
+            tcp_ack = tcp_hdr[3]
+            tcp_control_flags = tcp_hdr[5]
             # Check if TCP segment has SYN control bit  
-            #logging.info(f"{src_ip} / {tcp_dst_port}")
-            if src_ip  == "162.159.134.234":
-                logging.info(f"Control fields : {tcp_hdr[5]}")
-                logging.info(f"{tcp_hdr}")
+            if src_ip  == "192.168.3.104" and tcp_control_flags == 2:
+                # TODO : Generate Seq num , set ACK to ISN + 1 received from client 
+                logging.info(f"[Server] Received SYN, sending SYN-ACK")
+                logging.info(f"[Server] Client ACK = {tcp_ack}")
+                # Generate SYN + ACK packet 
+                syn_ack_pak = TCPPacket(
+                    dest_ip,
+                    20,
+                    src_ip,
+                    tcp_dst_port,
+                    0b000010010
+                )
+                seq = random.randint(1,1000) # randomly gen seq num
+                ack = tcp_ack + 1 # ACK = ISN + 1 
+                rcv_sock.sendto(syn_ack_pak.build(ack=ack, seq=seq),(src_ip,0))
+
 
             if tcp_hdr[5] == 2:
-                # TODO : Find out why I can't capture SYN packets ? 
                 logging.info(f"SYN packet detected : {tcp_hdr}\n{tcp_hdr[5]}")
 
-            #logging.info(f"tcp_header: {tcp_hdr}\nSource Port: {tcp_hdr[1]}\nDestination Port : {tcp_hdr[0]}\n")
 
-            #else:
-                #logging.info(f"tcp_header: {tcp_hdr}\nSource Port: {tcp_hdr[1]}\nDestination Port : {tcp_hdr[0]}\n")
 
 
 
