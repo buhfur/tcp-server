ac61b4ec60eccceef1ef507e3dba9c0bb4837094
root <ryanmcvicker@protonmail.com>
2025-01-10 23:32:22 -0500

    Auto Commit: Fri Jan 10 11:32:22 PM EST 2025

--- tcp-server/server.py
+++ tcp-server/server.py
@@ -89,7 +89,6 @@ def recv_pak(sock: socket.socket, client_ip: str, handshake_queue=handshake_queu
             # Disect IP header 
             packet = TCPPacket.build_pak(data)
             # Check if SYN + ACK was from server IP 
-            logging.info(f"[Server] dst_host = {packet.dst_host}\nclient_ip = {client_ip}")
             if packet.dst_host == client_ip : 
                 # Checks if packet has SYN control flag
                 if packet.flags == 2:
